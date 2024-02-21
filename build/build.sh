e0bf7522d692726929f146d0e3bd9f054d393c29
Federico Arienti <fa.arienti@gmail.com>
2024-02-21 20:08:06 -0300

    only allow -r if build is successful

--- build/build.sh
+++ build/build.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-build_name="";
+build_name=""; build_status=0;
 scene_path="two_cubes.scene.cpp";
 run_build=false;
 
 compile() {
     echo "compiling $build_name with scene $scene_path."
-    cmake .. -DBUILD=$build_name -DSCENE=$scene_path
-    make
-    echo "compiled $build_name with scene $scene_path." 
+    cmake .. -DBUILD=$build_name -DSCENE=$scene_path && make
+    build_status=$?
+    if [ $build_status == 0 ]; then
+        echo "compiled $build_name with scene $scene_path." 
+    fi
 }
 
 run() {
-    cd bin
-    ./$build_name
-    cd ..
+    if [ "$build_name" == "" ]; then
+        build_name="build"
+    fi
+    if [ $build_status == 0 ]; then
+        cd bin
+        ./$build_name
+        cd ..
+    fi
 }
 
 while getopts "bdtrs:" option; do
@@ -38,4 +45,4 @@ while getopts "bdtrs:" option; do
 done
 
 if [ "$build_name" != "" ]; then compile ; fi
-if [ $run_build = true ]; then run ; fi
+if [ $run_build == true ]; then run ; fi
