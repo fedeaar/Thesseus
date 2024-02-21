2e5341871e74cc09eaab71e4e07288faec336acf
Federico Arienti <fa.arienti@gmail.com>
2024-02-21 18:48:03 -0300

    change build process to use scenes

--- build/build.sh
+++ build/build.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
+
 build_name="";
+scene_path="two_cubes.scene.cpp";
+run_build=false;
+
 compile() {
-    echo "compiling $1."
-    cmake .. -DBUILD=$1
-    build_name=$1 
-    make 
+    echo "compiling $build_name with scene $scene_path."
+    cmake .. -DBUILD=$build_name -DSCENE=$scene_path
+    make
+    echo "compiled $build_name with scene $scene_path." 
 }
 
-while getopts ":b:d:t" option; do
-    case $option in
-        d)
-            compile "debug";;
-        t) 
-            compile "tests";;
-        b)  
-            compile "build";;
-    esac
-done
-if [ "$build_name" = "" ]; then compile "build" ; fi
+run() {
+    cd bin
+    ./$build_name
+    cd ..
+}
 
-OPTIND=1
-while getopts ":r" option; do
+while getopts "bdtrs:" option; do
     case $option in
-        r) 
-            echo "running $build_name."
-            cd bin
-            ./${build_name};
+        b)  
+            build_name="build"
+            ;;
+        d)
+            build_name="debug"
+            ;;
+        t) 
+            build_name="tests"
+            ;;
+        r)
+            run_build=true
+            ;;
+        s)
+            scene_path=${OPTARG}
             ;;
     esac
 done
+
+if [ "$build_name" != "" ]; then compile ; fi
+if [ $run_build = true ]; then run ; fi
