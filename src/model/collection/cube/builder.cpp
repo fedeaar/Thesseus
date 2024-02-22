1f666c2ff5fad818227da0f87bc28c606ed12905
Federico Arienti <fa.arienti@gmail.com>
2024-02-22 19:31:04 -0300

    add normals to cube and use sparse formatting of data to avoid conversions

--- src/model/collection/cube/builder.cpp
+++ src/model/collection/cube/builder.cpp
@@ -2,12 +2,13 @@
 #include "raw.h"
 
 Model<shaders::Default3d> models::cube::create_default3d_model() {
-  std::shared_ptr<void> cube_ptr((void*)&data, [](void*) {});  // is static
-  auto fmt_vec =
-      std::vector<Raw::attribute>(std::begin(format), std::end(format));
-  const Raw raw(cube_ptr, sizeof(data), fmt_vec);
-  Reformatter rfmt(raw, {{Reformatter::MOVE, move_action : {0}}});
-  auto raw_ptr = std::make_shared<Raw>(rfmt.get());
+  std::shared_ptr<void> cube_ptr((void*)&data, [](auto*) {});  // is static
+  std::vector<Raw::attribute> fmt_vec = {
+      {{gl_float, 3}, true},       // position
+      {{gl_float, 3}, true},       // normals (skip)
+      {{gl_float, 2}, true, true}  // texture coords (skip)
+  };
+  auto raw_ptr = std::make_shared<Raw>(cube_ptr, sizeof(data), fmt_vec);
   auto vao_ptr = std::make_shared<VAO>(raw_ptr);
   return Model<shaders::Default3d>(vao_ptr);
 }
@@ -15,20 +16,24 @@ Model<shaders::Default3d> models::cube::create_default3d_model() {
 Model<shaders::Default3dTextured>
 models::cube::create_default3dTextured_model() {
   std::shared_ptr<void> cube_ptr((void*)&data, [](auto*) {});  // is static
-  auto fmt_vec =
-      std::vector<Raw::attribute>(std::begin(format), std::end(format));
+  std::vector<Raw::attribute> fmt_vec = {
+      {{gl_float, 3}, true},        // position
+      {{gl_float, 3}, true, true},  // normals (skip)
+      {{gl_float, 2}, true}         // texture coords
+  };
   auto raw_ptr = std::make_shared<Raw>(cube_ptr, sizeof(data), fmt_vec);
   auto vao_ptr = std::make_shared<VAO>(raw_ptr);
   return Model<shaders::Default3dTextured>(vao_ptr);
 }
 
 Model<shaders::LightSource> models::cube::create_lightSource_model() {
-  std::shared_ptr<void> cube_ptr((void*)&data, [](void*) {});  // is static
-  auto fmt_vec =
-      std::vector<Raw::attribute>(std::begin(format), std::end(format));
-  const Raw raw(cube_ptr, sizeof(data), fmt_vec);
-  Reformatter rfmt(raw, {{Reformatter::MOVE, move_action : {0}}});
-  auto raw_ptr = std::make_shared<Raw>(rfmt.get());
+  std::shared_ptr<void> cube_ptr((void*)&data, [](auto*) {});  // is static
+  std::vector<Raw::attribute> fmt_vec = {
+      {{gl_float, 3}, true},        // position
+      {{gl_float, 3}, true, true},  // normals (skip)
+      {{gl_float, 2}, true, true}   // texture coords (skip)
+  };
+  auto raw_ptr = std::make_shared<Raw>(cube_ptr, sizeof(data), fmt_vec);
   auto vao_ptr = std::make_shared<VAO>(raw_ptr);
   return Model<shaders::LightSource>(vao_ptr);
 }
